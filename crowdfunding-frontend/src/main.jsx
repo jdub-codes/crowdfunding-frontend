2256fdaabbd58b3f66e6d400b69a775254425c16
jdub-codes <jenny.waller@riotinto.com>
2023-09-05 20:35:01 +1000

    frontend crowdfunding project

--- crowdfunding-frontend/src/main.jsx
+++ crowdfunding-frontend/src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from"react-router-dom"
+
+import HomePage from"./pages/HomePage.jsx"
+import ProjectPage from"./pages/ProjectPage.jsx"
+
+import NavBar from"./components/NavBar.jsx"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<NavBar />,
+    children: [
+      {path:"/",element:<HomePage />},
+      { path: "/project/:id", element: <ProjectPage /> }
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* Here we wrap our app in the router provider so they render */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
